1e72e198f3f1347caa6e65c05210a0e539e78f83
Fady2007 <96028450+Fady2007@users.noreply.github.com>
2023-12-31 20:23:55 +0200

    Update page.tsx

--- src/app/edit123/[id]/page.tsx
+++ src/app/edit123/[id]/page.tsx
@@ -2,9 +2,34 @@ import EditForm from "@/app/components/EditForm";
 import getTopicById from "../../../../models/functions";
 
 export default async function EditTopic({ params }: any) {
-    const { id } = params;
-    const { topic } = await getTopicById(id);
-    const { title, des } = topic;
-
     return <EditForm id={id} title={title} des={des} />;
+}
+
+// Fetch static paths for dynamic routes
+export async function getStaticPaths() {
+  // Example: Fetch IDs from an API or database
+  const ids = ['id1', 'id2', 'id3']; // Replace with your IDs
+
+  // Map IDs to the params object required by Next.js
+  const paths = ids.map((id) => ({ params: { id } }));
+
+  return {
+    paths,
+    fallback: false // Set to true if you have fallback behavior
+  };
+}
+
+// Fetch data for each individual page
+export async function getStaticProps({ params }: any) {
+  const { id } = params;
+  const { topic } = await getTopicById(id);
+  const { title, des } = topic;
+
+  return {
+    props: {
+      id,
+      title,
+      des
+    }
+  };
 }
